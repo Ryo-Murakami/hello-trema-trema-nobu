b9c4c0b60599e267621ea1127ea037c8313a014a
Yuji Nobuke <y-nobuke@ist.osaka-u.ac.jp>
2016-10-05 14:12:54 +0900

    スイッチ停止時の動作を規定

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,18 @@
 # Hello World!
 class HelloTrema < Trema::Controller
-  def start(args)
-    logger.info "Trema started (args = #{args.inspect})."
+  def start(_args)
+    logger.info 'Trema started.'
+    #tremaがスタートした時
   end
 
   def switch_ready(datapath_id)
-    logger.info format('Hello %#x!', datapath_id)
+    logger.info "Hello #{datapath_id.to_hex}!"
+    #スイッチが準備したときのハンドラ
+  end
+  
+  def switch_disconnected(datapath_id)
+    logger.info "Bye #{datapath_id.to_hex}"
+    #スイッチが停止した時のハンドラ
+    #停止方法は他のターミナルから trema stop
   end
 end
